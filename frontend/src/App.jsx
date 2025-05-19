a00d2fdd4f46bd24b2bfd113f0954b3645669dce
Guilherme <=>
2025-05-19 14:28:15 -0300

    iniciando projeto do frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Seja bem-vindo ao FinanceTracker</h1>
+      <p>Uma aplicação que vai ajudar você a controlar os gastos.</p>
+    </div>
+  );
+}
+
+export default App;
